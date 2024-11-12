c3203cb05dbc60880eb31c2e8a2e92319b3a19c4
NamaWorks <pedronayamartinez@hotmail.com>
2024-11-12 21:53:56 +0100

    prepare sections for Home page

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -16,11 +16,29 @@ const Home = () => {
 
       <section className='home-section' id='home-highlight-text'>
         <div className='home-hl-text-container'>
-          <p>Lorem ipsum dolor sit amet consectetur. Et diam tempus ultrices in ultricies amet felis. Vitae et nisi tellu.</p>
+          <h1>Lorem ipsum dolor sit amet consectetur. Et diam tempus ultrices in ultricies amet felis. Vitae et nisi tellu.</h1>
         </div>
       </section>
 
       <section className='home-section' id='home-drag-menu'></section>
+
+      <section className='home-section' id='home-about-us'>
+        <div className='home-about-us-container'>
+          <p>Lorem ipsum dolor sit amet consectetur. Amet maecenas consectetur vulputate nibh dignissim sed dolor aliquet tristique. Eu viverra tortor ut dui iaculis quis urna amet. Et a ultrices et laoreet placerat enim massa nunc. Dignissim aliquam mus aliquam aenean faucibus velit.</p>
+          <div>
+            <img src="/public/assets/img/illustrations/another-one.svg" alt="illustration of pouring wine" />
+          </div>
+        </div>
+        <div className='home-about-us-container'>
+          <img src="/public/assets/img/pictures/gondola-ride.png" alt="image of a gondola ride" />
+        </div>
+      </section>
+
+      <section className='home-section' id='home-top-sellers'>
+        <h4>top sellers</h4>
+      </section>
+
+
     </main>
     </>
   )
