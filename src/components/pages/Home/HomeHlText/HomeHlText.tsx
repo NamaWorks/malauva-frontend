5e1f52f3fb639fb845b494e55806561dc0310770
NamaWorks <pedronayamartinez@hotmail.com>
2024-11-14 20:56:02 +0100

    prepare HomeHlText and HomeHero sections

--- src/components/pages/Home/HomeHlText/HomeHlText.tsx
+++ src/components/pages/Home/HomeHlText/HomeHlText.tsx
@@ -1,4 +1,4 @@
-import './HomeHlText.scss'
+import "./HomeHlText.scss";
 import React from "react";
 
 const HomeHlText = () => {
@@ -7,8 +7,23 @@ const HomeHlText = () => {
       <section className="home-section" id="home-highlight-text">
         <div className="home-hl-text-container">
           <h2>
-            Lorem ipsum dolor<span> <img src="/public/assets/img/illustrations/old-man-drinking.svg" alt="old man drinking a glass of wine" /></span> sit amet consectetur. Et diam <span><img src="/public/assets/img/illustrations/flowers.svg" alt="flowers illustration" /></span> tempus ultrices in
-            ultricies amet felis. Vitae et nisi tellu.
+            Lorem ipsum dolor
+            <span className="home-hl-illustrations">
+              <img
+                id="home-hl-old-ill"
+                src="/public/assets/img/illustrations/old-man-drinking.svg"
+                alt="old man drinking a glass of wine"
+              />
+            </span>
+            sit amet consectetur. Et diam
+            <span className="home-hl-illustrations">
+              <img
+                id="home-hl-flowers-ill"
+                src="/public/assets/img/illustrations/flowers.svg"
+                alt="flowers illustration"
+              />
+            </span>{" "}
+            tempus ultrices in ultricies amet felis. Vitae et nisi tellu.
           </h2>
         </div>
       </section>
