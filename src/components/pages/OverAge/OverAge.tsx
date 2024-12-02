bfbbe5a34d51db1713cd771811fd14f540141aeb
NamaWorks <pedronayamartinez@hotmail.com>
2024-12-02 22:45:32 +0100

    prepare overage page

--- src/components/pages/OverAge/OverAge.tsx
+++ src/components/pages/OverAge/OverAge.tsx
@@ -9,13 +9,17 @@ const OverAge = () => {
 
       <div className='overage-container'>
         <div className='overage-content-div'>
-          
-        </div>
-        <div className='overage-content-div'>
           <img src="/public/assets/img/illustrations/glasses.svg" alt="illustrations of stored glasses" />
         </div>
+        <div className='overage-content-div'>
+          <p>+21?</p>
+          <div className='overage-buttons-container'>
+            <button className="overage-button" onClick={()=>{console.log('button clicked')}}>Y</button>
+            <p>/</p>
+            <button className="overage-button" onClick={()=>{console.log('button clicked')}}>N</button>
+          </div>
+        </div>
       </div>
-
 
     </main>
     
