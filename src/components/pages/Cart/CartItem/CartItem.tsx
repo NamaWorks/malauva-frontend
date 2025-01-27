3c8f86e8099b81a9012d11ff6c0425425f9f4c4b
NamaWorks <pedronayamartinez@hotmail.com>
2025-01-27 11:43:52 +0100

    add temporal solution to cartItems disappearing all at once when reached 0 items on any of the itemCount

--- src/components/pages/Cart/CartItem/CartItem.tsx
+++ src/components/pages/Cart/CartItem/CartItem.tsx
@@ -14,38 +14,40 @@ const CartItem = ({item, updateData, setUpdateData}: {item: cartReadyItem}) => {
     changeNumberOfItemsInUserCart(item.idNumber, itemsNumber)
   },[itemsNumber])
 
-  return (
-    <>
-
-    <div className='cart-item' id={`cart-item-${item.idNumber}`}>
-
-  <div className='cart-item-horizontal-container'>
-      <div className="cart-item-img">
-        <img src={item.picture} alt={`${item.idNumber}-image`} />
-      </div>
-
-      <div className='cart-item-data'>
-        <h5 className="cart-item-name">{item.name}</h5>
-        <BodyCopy text={`Item ID: ${item.idNumber}`} />
-      </div>
-  </div>
-
-  <div className="cart-item-horizontal-container">
-    <div className='add-more-btn'>
-        <button onClick={()=>{setItemsNumber(itemsNumber-1); setUpdateData(!updateData)}}>-</button>
-          <p>{itemsNumber}</p>
-        <button onClick={()=>{setItemsNumber(itemsNumber+1); setUpdateData(!updateData)}}>+</button>
+  if(itemsNumber>0){
+    return (
+      <>
+  
+      <div className='cart-item' id={`cart-item-${item.idNumber}`}>
+  
+    <div className='cart-item-horizontal-container'>
+        <div className="cart-item-img">
+          <img src={item.picture} alt={`${item.idNumber}-image`} />
+        </div>
+  
+        <div className='cart-item-data'>
+          <h5 className="cart-item-name">{item.name}</h5>
+          <BodyCopy text={`Item ID: ${item.idNumber}`} />
+        </div>
     </div>
-  </div>
-
-  <div className="cart-item-horizontal-container">
-    <Sublink text='Remove all' color='dark' fnc={()=>{setItemsNumber(0); setUpdateData(!updateData)}}/>
-    <PriceText value={item.price * itemsNumber} background={true} extraClass='cart-price'/>
-  </div>
-
+  
+    <div className="cart-item-horizontal-container">
+      <div className='add-more-btn'>
+          <button onClick={()=>{itemsNumber==0 ? setItemsNumber(0) : setItemsNumber(Number(itemsNumber)-1); setUpdateData(!updateData)}}>-</button>
+            <p>{itemsNumber}</p>
+          <button onClick={()=>{setItemsNumber(Number(itemsNumber)+1); setUpdateData(!updateData)}}>+</button>
+      </div>
     </div>
-    </>
-  )
+  
+    <div className="cart-item-horizontal-container">
+      <Sublink text='Remove all' color='dark' fnc={()=>{setItemsNumber(0); setUpdateData(!updateData)}}/>
+      <PriceText value={item.price * itemsNumber} background={true} extraClass='cart-price'/>
+    </div>
+  
+      </div>
+      </>
+    )
+  }
 }
 
 export default CartItem
--- src/utils/functions/api_fn/changeNumberOfItemsInUserCart.ts
+++ src/utils/functions/api_fn/changeNumberOfItemsInUserCart.ts
@@ -3,14 +3,18 @@ import { fetchData } from "./fetchData"
 
 export const changeNumberOfItemsInUserCart = async(productIdNumber: String, numberToSet:number) => {
 
-  const user = await fetchData("/users/cart")
+    const user = await fetchData("/users/cart")
   const userId = user._id
   let itemToModify:Number
 
   user.cartItems.forEach((item, i) => {
     if(item.includes(productIdNumber)){itemToModify=i}
   });
-  const parsedItem = JSON.parse(user.cartItems[itemToModify])  
+  const parsedItem = JSON.parse(user.cartItems[itemToModify])      
+
+  console.log(user)
+
+
 
   if(numberToSet>0){
     const newItem = {...parsedItem, numberOfItems: numberToSet}
@@ -25,18 +29,35 @@ export const changeNumberOfItemsInUserCart = async(productIdNumber: String, numb
         body: JSON.stringify(payload)
       })
   } else {
-    const newArr = user.cartItems.splice(itemToModify,1)
-    const data = await fetch(apiUrl+'/users/update/'+userId, {
-      headers:{
-        "Content-type": "application/json",
-        "Authorization": `Bearer ${sessionStorage.getItem('token')}`
-      },
-      method: "PATCH",
-      body: JSON.stringify(newArr)
-    })
-    console.log(data)
+    try {
+      // user.cartItems.splice(itemToModify,1)
+
+      // console.log(user.cartItems)
+  
+      // const data = await fetch(apiUrl+'/users/update/'+userId, {
+      //   headers:{
+      //     "Content-type": "application/json",
+      //     "Authorization": `Bearer ${sessionStorage.getItem('token')}`
+      //   },
+      //   method: "PATCH",
+      //   body: JSON.stringify(user.cartItems)
+      // })
+
+      const newItem = {...parsedItem, numberOfItems: 0}
+      user.cartItems[itemToModify] = JSON.stringify(newItem)
+      const payload = { cartItems: user.cartItems }
+        const data = await fetch(apiUrl+'/users/update/'+userId, {
+          headers:{
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${sessionStorage.getItem('token')}`
+          },
+          method: "PATCH",
+          body: JSON.stringify(payload)
+        })
+
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    }
   }
-
-
-
 }
