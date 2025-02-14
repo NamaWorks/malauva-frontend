28f9a2940ca1e49d46cdf772be2adb82abe78f10
NamaWorks <pedronayamartinez@hotmail.com>
2025-02-14 10:41:10 +0100

    change apiUrl

--- src/data/globalVariables.ts
+++ src/data/globalVariables.ts
@@ -1,2 +1,2 @@
-// export const apiUrl: string = "https://malauva-backend.vercel.app";
-export const apiUrl: string = "http://localhost:3000";
+export const apiUrl: string = "https://malauva-backend.vercel.app";
+// export const apiUrl: string = "http://localhost:3000";
